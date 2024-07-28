99a2ed55153c7811a0934a0e72999ac4a3062c6f
Sunsu Kim <tansol1024@naver.com>
2024-07-28 12:14:12 +0900

    Read CSV _None_Header파일

--- 02_Csv_read_None_Header.py
+++ 02_Csv_read_None_Header.py
@@ -10,7 +10,7 @@ df=pd.read_csv(file) # option 없이 DataFrame 생성
 print(df.head(2)) # 첫번째 자료가 columns로 전환됨.
 
 df=pd.read_csv(file,header=None) # 'head=' option None으로 설정
-print(df.head(2)) # Index Class로 column이 지정됨
+print(df.head(2)) # Range Index로 column이 지정됨
 
 # 컬럼 지정 : header=None,names=[컬럼명,,,,,,,]
 df=pd.read_csv(file,header=None,names=['ID','이름','생일','학과','영어','일본어','중국어'])
